d2eaf7cf74d4df8f4c44f2d8817835ca56bb00db
pavan <balubadipavan@gmail.com>
2024-04-05 22:36:28 +0530

    learn jenkins shared ci groovy

---
+++ jenkins-ip-update/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "bpk-terra-state"
+    key    = "misc/jenkins/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "jenkins" {
+  instance_id = "i-0a7740678d85b3256"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins"
+  type    = "A"
+  zone_id = "Z0884885ZSESFET2B3ZQ"
+  ttl     = 10
+  records = [data.aws_instance.jenkins.public_ip]
+}
